a84272b2c43e37a5c68b970a0f6774c16485434c
Jonathan Bowman <jonathanrbowman@me.com>
2013-11-27 17:43:58 -0500

    Heroku and Devise have both been configured

--- Gemfile
+++ Gemfile
@@ -47,3 +47,5 @@ end
 gem 'rails_12factor', group: :production
 
 ruby "2.0.0"
+
+gem 'devise', '~> 3.1.1'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Gaicd10rails::Application.routes.draw do
  
+  devise_for :users
   # You can have the root of your site routed with "root"
    root 'pages#home'
 
