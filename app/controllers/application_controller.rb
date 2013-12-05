5a84cf745c867d05a914d233adda7f8c2139782b
Jonathan Bowman <jonathanrbowman@me.com>
2013-12-05 12:45:08 -0500

    Made sign in page default and added redirect routes after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,21 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_filter :authenticate_user!
+  def after_sign_in_path_for(resource_or_scope)
+    if current_user.admin?
+      admin_view_path
+    else
+    tasks_path
+    end
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 
   protected
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :username
     devise_parameter_sanitizer.for(:sign_up) << :user_company
--- config/routes.rb
+++ config/routes.rb
@@ -1,66 +1,65 @@
 Gaicd10rails::Application.routes.draw do
- 
-  
+
   resources :tasks do
-    member do 
+    member do
       post 'status_change'
     end
-end
+  end
 
   devise_for :users, :controllers => {:registrations => "registrations", :devise => "devise"}
   
-   root 'pages#home'
-   
-   get '/tasks/:id/status_change' => "tasks#status_change"
-   get '/admin_view' => "tasks#admin_index"
-   get '/admin_detail' => "tasks#admin_hospital_detail"
+  root 'pages#home'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get '/tasks/:id/status_change' => "tasks#status_change"
+  get '/admin_view' => "tasks#admin_index"
+  get '/admin_detail' => "tasks#admin_hospital_detail"
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+# Example of regular route:
+#   get 'products/:id' => 'catalog#view'
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+# Example of named route that can be invoked with purchase_url(id: product.id)
+#   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+# Example resource route (maps HTTP verbs to controller actions automatically):
+#   resources :products
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+# Example resource route with options:
+#   resources :products do
+#     member do
+#       get 'short'
+#       post 'toggle'
+#     end
+#
+#     collection do
+#       get 'sold'
+#     end
+#   end
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-  
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
+# Example resource route with sub-resources:
+#   resources :products do
+#     resources :comments, :sales
+#     resource :seller
+#   end
 
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+# Example resource route with more complex sub-resources:
+#   resources :products do
+#     resources :comments
+#     resources :sales do
+#       get 'recent', on: :collection
+#     end
+#   end
+
+# Example resource route with concerns:
+#   concern :toggleable do
+#     post 'toggle'
+#   end
+#   resources :posts, concerns: :toggleable
+#   resources :photos, concerns: :toggleable
+
+# Example resource route within a namespace:
+#   namespace :admin do
+#     # Directs /admin/products/* to Admin::ProductsController
+#     # (app/controllers/admin/products_controller.rb)
+#     resources :products
+#   end
 end
