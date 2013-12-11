e6c54a6ccfa25dda78e446663e9ed826590715a7
Jonathan Bowman <jonathanrbowman@me.com>
2013-12-10 21:49:21 -0500

    addressed some styling issues with buttons

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -40,6 +40,7 @@ class RegistrationsController < DeviseController
   # We need to use a copy of the resource because we don't want to change
   # the current user in place.
   def update
+    
     self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
     prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
 
