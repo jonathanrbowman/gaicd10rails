d2781ca186202d4e3bbaf1aba552560069323840
Jonathan Bowman <jonathanrbowman@me.com>
2014-01-16 20:37:31 -0500

    Changed contact submit logic to account for nil entries

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,12 @@ class PagesController < ApplicationController
   end
   
   def submit_contact_request
+    if params[:contact_email] != nil
     @contact_result = params[:contact_email]
     UserNotifications.new_signup(@contact_result).deliver
+    else
+      return
+    end
   end
   
 end
